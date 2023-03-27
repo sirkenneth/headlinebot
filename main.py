3ad762900c359e84f47378003e722be5a824773e
Kenneth Dée <68607947+sirkenneth@users.noreply.github.com>
2023-03-28 00:19:07 +0200

    First test

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import os
-import telebot
+import telebot   #import api wrapper
 
+bot = telebot.TeleBot("5853259409:AAFuy-OQ4tsmO6vP_XLK4uuZaBuyCLqYtnE", parse_mode=None)
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
